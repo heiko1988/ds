da4ee0dd2febc2ea5ce8eadef6ee393b934dd082
heiko1988 <chaos4live1988@gmail.com>
2025-12-24 08:41:03 +0100

    Use optional chaining for color extraction

--- farmgod_test.js
+++ farmgod_test.js
@@ -657,7 +657,7 @@ window.FarmGod.Main = (function (Library, Translation) {
           $el.find('a[href*="screen=report&mode=all&view="]').first().text().toCoord()
         ] = {
           id: $el.attr('id').split('_')[1].toNumber(),
-          color: $el.find('img[src*="graphic/dots/"]').attr('src').match(/dots\/(green|yellow|red|blue|red_blue)/)[1],
+          color: $el.find('img[src*="graphic/dots/"]').attr('src')?.match(/dots\/(green|yellow|red|blue|red_blue)/)?.[1] || null,
           max_loot: $el.find('img[src*="max_loot/1"]').length > 0,
         });
       });
